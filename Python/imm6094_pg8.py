2490d0ec1eeaddbc2113384ed839a4490e53edd1
manoranjanmishra <manoranjanmishra@iitb.ac.in>
2014-07-11 16:43:11 +0530

    Added a find hashad number function
    THe find_Hashad function finds all numbers upto a limit n that you must
    specify.

--- Python/imm6094_pg8.py
+++ Python/imm6094_pg8.py
@@ -1,3 +1,4 @@
+#problem-1
 def ishashad(n):
 	#an integer div by the sum of it's digits is a hashad number
 	a="%d"%(n)
@@ -11,5 +12,16 @@ def ishashad(n):
 		return True
 	else:
 		return False
+def findhashad(n):
+	#given an upper limit n, the function determines all hashad numbers upto n, and also counts them.
+	c=0
+	i=1
+	while(i<=n):
+		if ishashad(i)==True:
+			print i,"\t",
+			c=c+1
+		i=i+1
+	print "\nThere are",c,"Hashad numbers upto",n,"starting from 1"
 #while True:
 #	print ishashad(int(raw_input()))#for testing purpose uncomment these lines
+#findhashad(int(raw_input()))
